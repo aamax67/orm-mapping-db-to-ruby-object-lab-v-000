cb1701afef1a642ee1dfbacad7813979ab0aeccb
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-03-21 02:01:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,6 +87,20 @@ class Student
     end.first
   end
 
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = X
+    ORDER BY students.id
+    LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
 
   def save
     sql = <<-SQL
